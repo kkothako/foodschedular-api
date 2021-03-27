1345ce694a5450be3c4d9e01d5c4c3cbdc1fd418
krishna k <kittu.1214@gmail.com>
2021-03-26 21:56:25 -0400

    Get draft order changes

--- api/router/order-draft.router.js
+++ api/router/order-draft.router.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const orderDraftController = require('../controller/order-draft.controller');
 
 router.post('/orderDraft', orderDraftController.createDraftOrder);
+router.post('/orderDraft/getOrderByProfileID', orderDraftController.getOrderByProfileID);
 
 module.exports = router;
