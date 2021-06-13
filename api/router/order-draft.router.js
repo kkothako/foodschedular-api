1f15cbe4448eaf574560db80537b5311f3e05c9b
krishna k <kittu.1214@gmail.com>
2021-06-12 21:31:40 -0400

    Get Call  changed to Post

--- api/router/order-draft.router.js
+++ api/router/order-draft.router.js
@@ -6,7 +6,7 @@ const orderDraftController = require('../controller/order-draft.controller');
 router.post('/orderDraft', orderDraftController.createDraftOrder)
 .post('/orderDraft/getOrderByProfileID', orderDraftController.getOrderByProfileID)
 .post('/orderDraft/deleteDraftOrderByOrderId', orderDraftController.deleteDraftOrderByOrderId)
-.get('/orderDraft/getLangAndLatBy', orderDraftController.getRestorentDistanceBy);
+.post('/orderDraft/getLangAndLatBy', orderDraftController.getRestorentDistanceBy);
 
 
 module.exports = router;
